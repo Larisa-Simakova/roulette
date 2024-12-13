1ef79803a8c4ab81f2f2814a29a1ca86c39cdca6
Larisa-Simakova <larica.simakova@gmail.com>
2024-12-13 19:37:03 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Loading================================================================================*/
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('hidden');
+});
+
+/*Content_download================================================================================*/
+let wrapper = document.querySelector('.wrapper');
+window.addEventListener('load', (event) => {
+	wrapper.classList.add('loaded');
+});
